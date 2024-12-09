88171a91b9e2cb08146261745add24670f290967
revathi_gollapudi <revathi7461@gmail.com>
2024-12-08 19:53:41 -0500

    wrote Dbcontext and database connection were succesfullt made

--- Controllers/FormController.cs
+++ Controllers/FormController.cs
@@ -1,21 +1,79 @@
 ﻿using dotNetProject.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using Microsoft.SqlServer.Server;
+using Npgsql;
 
 namespace dotNetProject.Controllers
 {
     public class FormController : Controller
     {
         private readonly ILogger<FormController> _logger;
+        private readonly DbContextDatabase _context;
 
-        public FormController(ILogger<FormController> logger)
+        public FormController(ILogger<FormController> logger, DbContextDatabase context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult FormView()
         {
-            return View();
+            var FormData = new FormDataModal();
+
+            //string connectionString = "Host=localhost;port=5432;Database=form_data;Username=form_data;Password=form_data";
+            //FormDataModal? users = null;
+
+            //using (NpgsqlConnection conn = new(connectionString))
+            //{
+            //    conn.Open();
+            //    Console.WriteLine("connection is Open");
+            //    string sqlQuery = "SELECT * FROM public.user_data ORDER BY user_id ASC LIMIT 1;";
+            //    NpgsqlCommand cmd = new(sqlQuery, conn);
+            //    NpgsqlDataReader reader = cmd.ExecuteReader();
+
+            //    if (reader.HasRows)
+            //    {
+            //        Console.WriteLine("There are rows");
+
+            //        while (reader.Read())
+            //        {
+            //            users = new FormDataModal
+            //            {
+            //                UserId = reader.GetInt32(0),
+            //                FirsName = reader.GetString(1),
+            //                MiddleName = reader.GetString(2),
+            //                LastName = reader.GetString(3),
+            //                Email = reader.GetString(4),
+            //                Phone = reader.GetString(5),
+            //                Age = reader.GetInt32(6)
+            //            };
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("No rows found.");
+            //    }
+            //}
+
+                return View(FormData);
         }
+
+        // POST method for inserting data into database
+        // Triggered when 'submit' is cliced in 'create user' page
+        // Form/InsertUser POST method
+
+        public IActionResult InsertUser(FormDataModal formData)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.UserData.Add(formData);
+                _context.SaveChanges();
+
+                ViewBag.showModal = true;
+                return View("FormView", formData);
+            }
+            return View("FormView", formData);
+
+        }   
     }
 }
---
+++ Models/FormDataModal.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace dotNetProject.Models
+{
+    public class FormDataModal
+    {
+        [Column("user_id")]
+        public int UserId {  get; set; }
+        [Column("first_name")]
+        public string? FirstName { get; set; }
+        [Column("middle_name")]
+        public string? MiddleName { get; set; }
+        [Column("last_name")]
+        public string? LastName { get; set;}
+        [Column("email")]
+        public string? Email { get; set; }
+        [Column("phone")]
+        public string? Phone { get; set; }
+        [Column("date_of_birth")]
+        public DateOnly? DateOfBirth { get; set; }
+        [Column("age")]
+        public int? Age { get; set; }
+        [Column("nationality")]
+        public string? Nationality {  get; set; }
+        [Column("occupation")]
+        public string? Occupation { get; set; }
+        [Column("address1")]
+        public string? Address1 { get; set;}
+        [Column("address2")]
+        public string? Address2 { get; set; }
+        [Column("city")]
+        public string? City { get; set; }
+        [Column("state")]
+        public string? State { get; set; }
+        [Column("country")]
+        public string? Country { get; set; }
+        [Column("pincode")]
+        public string? Pincode { get; set; }
+        [Column("degree")]
+        public string? Degree { get; set; }
+        [Column("institution")]
+        public string? Institution { get; set; }
+        [Column("year_completed")]
+        public int? YearCompleted { get; set; }
+
+    }
+}
